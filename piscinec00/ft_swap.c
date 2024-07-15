4f991fce6a225b34cad4cea11f4710ee102f97d6
naiara-ada <naiara.ada@gmail.com>
2024-07-15 19:33:04 +0200

    ft_swap added

---
+++ piscinec00/ft_swap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int		tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int main()
+{
+	int x = 5;
+	int y = 10;
+
+	printf("Before swap: x = %d, y = %d\n", x, y);
+	ft_swap(&x, &y);
+	printf("After swap: x = %d, y = %d\n", x, y);
+	
+	return 0;
+}
