44702aa5c0cbcf04cf84061ff4a82a9c53506220
naiara-ada <naiara.ada@gmail.com>
2024-07-15 19:37:03 +0200

    added ft_ft and rename ft_ultimate

--- piscinec00/ft_ft.c
+++ piscinec00/ft_ft.c
@@ -1,25 +1,16 @@
 #include <stdio.h>
 
-void	ft_ultimate_ft(int *********nbr)
+void ft_ft(int *nbr)
 {
-	*********nbr = 42;
+    *nbr = 42;
 }
 
-int main(void)
+int main()
 {
-	int number;
-	int *ptr1 = &number;
-	int **ptr2 = &ptr1;
-	int ***ptr3 = &ptr2;
-	int ****ptr4 = &ptr3;
-	int *****ptr5 = &ptr4;
-	int ******ptr6 = &ptr5;
-	int *******ptr7 = &ptr6;
-	int ********ptr8 = &ptr7;
-	int *********ptr9 = &ptr8;
+    int number; 
 
-	ft_ultimate_ft(ptr9);
-	printf("The value of number is: %d\n", number);
-	
-	return 0;
+    ft_ft(&number); 
+    printf("The value of number is: %d\n", number); 
+
+    return 0;
 }
